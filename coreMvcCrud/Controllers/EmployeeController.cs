4595e5166f71e18b32875e7fd7986b619beceb86
habibalt <hbaltaci@agdas.com.tr>
2022-08-19 17:03:07 +0300

    Initial commit

---
+++ coreMvcCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,79 @@
+﻿using coreMvcCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace coreMvcCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(Repository.AllEmpoyees);
+        }
+        public IActionResult Create()
+        {
+            
+            //Creating the List of SelectListItem, this list you can bind from the database.
+            List<SelectListItem> departments = new()
+            {
+                new SelectListItem { Value = "1", Text = "Development" },
+                new SelectListItem { Value = "2", Text = "QA" ,Selected=true},
+                new SelectListItem { Value = "3", Text = "Test" },
+                new SelectListItem { Value = "4", Text = "Analysis" },
+                new SelectListItem { Value = "5", Text = "Devops" },
+                new SelectListItem { Value = "6", Text = "HelpDesk" },
+                
+            };
+
+            //assigning SelectListItem to view Bag
+            ViewBag.departments = departments;
+            return View();
+        }
+  
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Create(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(string empname)
+        {
+            Employee employee = Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            Repository.Delete(employee);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(string empname)
+        {
+            Employee employee=Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult Update(Employee employee, string empname)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Age = employee.Age;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Salary = employee.Salary;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Department = employee.Department;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Sex = employee.Sex;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Name = employee.Name;
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+        public IActionResult ViewRecord(string empname)
+        {
+            Employee employee=Repository.AllEmpoyees.Where(e=>e.Name == empname).FirstOrDefault();    
+            return View(employee);
+        }
+    }
+}
---
+++ coreMvcCrud/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace coreMvcCrud.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "İsim boş geçilemez!")]
+        public string Name { get; set; }
+        [Range(15, 70, ErrorMessage = "Sadece 15-70 yaş arası değer!")]
+        public int Age { get; set; }
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Invalid, enter like # or #.##")]
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+
+        [RegularExpression(@"^[MF]+$", ErrorMessage = "Sadece tek cinsiyet seçimi!")]
+        public char Sex { get; set; }
+    }
+}
